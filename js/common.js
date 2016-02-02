a97cd27edf4005a5a5119564abf34a0ad86b7a5e
gvenvivar <gven.inc@gmail.com>
2016-02-02 16:34:03 +0200

    optimize

--- js/common.js
+++ js/common.js
@@ -1,55 +1,4 @@
-$(document).ready(function() {
-
-	function heightDetect() {
-		$("header").css("min-height", $(window).height());
-	};
-	heightDetect();
-	$(window).resize(function() {
-		heightDetect();
-	});
-
-
-	$(".tab_item").not(":first").hide();
-	$(".wrapper .tab").click(function() {
-	$(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
-	$(".tab_item").hide().eq($(this).index()).fadeIn()
-	}).eq(0).addClass("active");
-
-	$(".tab_item2").not(":first").hide();
-	$(".wrapper-2 .tab").click(function() {
-	$(".wrapper-2 .tab").removeClass("active").eq($(this).index()).addClass("active");
-	$(".tab_item2").hide().eq($(this).index()).fadeIn()
-	}).eq(0).addClass("active");
-
-	$(".tab_item3").not(":first").hide();
-	$(".wrapper-3 .tab").click(function() {
-	$(".wrapper-3 .tab").removeClass("active").eq($(this).index()).addClass("active");
-	$(".tab_item3").hide().eq($(this).index()).fadeIn()
-	}).eq(0).addClass("active");
-
-	$(".tab_item4").not(":first").hide();
-	$(".wrapper-4 .tab").click(function() {
-	$(".wrapper-4 .tab").removeClass("active").eq($(this).index()).addClass("active");
-	$(".tab_item4").hide().eq($(this).index()).fadeIn()
-	}).eq(0).addClass("active");
-
-	var owl = $('.owl-carousel');
-	 $(".owl-carousel").owlCarousel({
-		 	loop:true,
-	    	margin:10,
-	    	items:1,
-	    	responsiveClass:true,
-	    	center:true,
-	    	nav: true,
-	    	navText: '',
-	    	responsive:{
-	    		0:{
-		            items:1,
-		            nav:true
-		        }
-	    	}
-    	}		
-    );
+$(function() {
 
 	//SVG Fallback
 	if(!Modernizr.svg) {
@@ -58,20 +7,34 @@ $(document).ready(function() {
 		});
 	};
 
-	//Аякс отправка форм
-	//Документация: http://api.jquery.com/jquery.ajax/
-	$("#form").submit(function(e) {
-		e.preventDefault;
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
 		$.ajax({
 			type: "POST",
-			url: "mail.php",
-			data: $(this).serialize()
+			url: "mail.php", //Change
+			data: th.serialize()
 		}).done(function() {
-			alert("Спасибо за заявку!");
+			alert("Thank you!");
 			setTimeout(function() {
-				$.fancybox.close();
+				// Done Functions
+				th.trigger("reset");
 			}, 1000);
 		});
+		return false;
 	});
-	
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
 });
